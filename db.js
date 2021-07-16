ff3e4c29bb1e2222c7e31a13f91070cf9b1a69aa
Venkat <pvenkat01101@gmail.com>
2021-07-16 13:45:50 +0530

    db password

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const Pool=require('pg').Pool
 
 const pool=new Pool({
     user:"rnakfaybnqbmqo",
-    password:"93317da9a6ffb327b9d7d305cfd04f00c069ff476893b2c39b79de9044da6fe1",
+    password:"f526d238e9c41351e4273036b8e8ecefb21dae8f4aa4ff02f319fcd93aa38a91",
     host:"ec2-52-2-118-38.compute-1.amazonaws.com",
     port:5432,
     database:"dd849lpevuadui"
