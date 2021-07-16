8fee1f7f239ee54a30b218dbb4b4af77ea307e19
Venkat <pvenkat01101@gmail.com>
2021-07-16 14:09:32 +0530

    db removed ssl

--- db.js
+++ db.js
@@ -7,7 +7,8 @@ const pool=new Pool({
     password:"f526d238e9c41351e4273036b8e8ecefb21dae8f4aa4ff02f319fcd93aa38a91",
     host:"ec2-52-2-118-38.compute-1.amazonaws.com",
     port:5432,
-    database:"dd849lpevuadui"
+    database:"dd849lpevuadui",
+    ssl: { rejectUnauthorized: false }
 })
 
 
